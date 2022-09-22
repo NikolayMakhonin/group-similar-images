a11f4ae5003bb78c3e676486af5fe816ee1a6a1b
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2022-09-22 12:36:27 +0400

    module template. All Tests OK

---
+++ dist/lib/index.cjs
@@ -0,0 +1,9 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+function test() {
+    console.log('test');
+}
+
+exports.test = test;
---
+++ src/index.test.ts
@@ -0,0 +1,7 @@
+import {test} from 'src/index'
+
+describe('index', async function () {
+  it('variants', async function () {
+    test()
+  })
+})
